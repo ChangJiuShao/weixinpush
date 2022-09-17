ef8c61959005bd8a7e2fd5422838b3bbcb707e40
gitrebase <51101923+ChangJiuShao@users.noreply.github.com>
2022-09-17 15:42:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ from datetime import datetime, date
 from zhdate import ZhDate
 import sys
 import os
- 
- 
+
+
 def get_color():
     # 获取随机颜色
     get_colors = lambda n: list(map(lambda i: "#" + "%06x" % random.randint(0, 0xFFFFFF), range(n)))
     color_list = get_colors(100)
     return random.choice(color_list)
- 
- 
+
+
 def get_access_token():
     # appId
     app_id = config["app_id"]
@@ -29,8 +29,8 @@ def get_access_token():
         sys.exit(1)
     # print(access_token)
     return access_token
- 
- 
+
+
 def get_weather(region):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
@@ -51,16 +51,39 @@ def get_weather(region):
         # 获取地区的location--id
         location_id = response["location"][0]["id"]
     weather_url = "https://devapi.qweather.com/v7/weather/now?location={}&key={}".format(location_id, key)
+    weather_url_3d = "https://devapi.qweather.com/v7/weather/3d?location={}&key={}".format(location_id, key)
+
+    response_3d = get(weather_url_3d, headers=headers).json()
     response = get(weather_url, headers=headers).json()
+
+    daily_arr = response_3d['daily']
+    now = daily_arr[0]
+
+    now_min = now['tempMin']
+    now_max = now['tempMax']
+
+    list_top_tmp = []
+    list_down_tmp = []
+
+    for day in daily_arr:
+        list_down_tmp.append(day['tempMin'])
+        list_top_tmp.append(day['tempMax'])
+    list_min = sorted(list_down_tmp)
+    list_max = sorted(list_top_tmp)
+    min = list_min[0]
+    max = list_max[len(list_max) - 1]
     # 天气
     weather = response["now"]["text"]
     # 当前温度
-    temp = response["now"]["temp"] + u"\N{DEGREE SIGN}" + "C"
+    temp = '最低' + now_min + u"\N{DEGREE SIGN}" + "C　最高 " + now_max + u"\N{DEGREE SIGN}" + "C"
     # 风向
     wind_dir = response["now"]["windDir"]
-    return weather, temp, wind_dir
- 
- 
+    # 未来三天
+    future_temp = '最低' + min + u"\N{DEGREE SIGN}" + "C　最高 " + max + u"\N{DEGREE SIGN}" + "C"
+
+    return weather, temp, wind_dir, future_temp
+
+
 def get_birthday(birthday, year, today):
     birthday_year = birthday.split("-")[0]
     # 判断是否为农历生日
@@ -78,7 +101,7 @@ def get_birthday(birthday, year, today):
         birthday_day = birthday.day
         # 今年生日
         year_date = date(year, birthday_month, birthday_day)
- 
+
     else:
         # 获取国历生日的今年对应月和日
         birthday_month = int(birthday.split("-")[1])
@@ -100,8 +123,8 @@ def get_birthday(birthday, year, today):
         birth_date = year_date
         birth_day = str(birth_date.__sub__(today)).split(" ")[0]
     return birth_day
- 
- 
+
+
 def get_ciba():
     url = "http://open.iciba.com/dsapi/"
     headers = {
@@ -113,9 +136,10 @@ def get_ciba():
     note_en = r.json()["content"]
     note_ch = r.json()["note"]
     return note_ch, note_en
- 
- 
-def send_message(to_user, access_token, region_name, weather, temp, wind_dir, note_ch, note_en):
+
+
+def send_message(to_user, access_token, region_name, weather, temp, wind_dir, note_ch, note_en, future_temp,
+                 test_day_str):
     url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
     week_list = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
     year = localtime().tm_year
@@ -157,12 +181,20 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
                 "value": temp,
                 "color": get_color()
             },
+            "future_temp": {
+                "value": future_temp,
+                "color": get_color()
+            },
             "wind_dir": {
                 "value": wind_dir,
                 "color": get_color()
             },
             "love_day": {
                 "value": love_days,
+                "color": get_color()
+            },
+            "test_day": {
+                "value": test_day_str,
                 "color": get_color()
             },
             "note_en": {
@@ -172,7 +204,7 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
             "note_ch": {
                 "value": note_ch,
                 "color": get_color()
-            }
+            },
         }
     }
     for key, value in birthdays.items():
@@ -200,8 +232,8 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
         print("推送消息成功")
     else:
         print(response)
- 
- 
+
+
 if __name__ == "__main__":
     try:
         with open("config.txt", encoding="utf-8") as f:
@@ -214,20 +246,26 @@ if __name__ == "__main__":
         print("推送消息失败，请检查配置文件格式是否正确")
         os.system("pause")
         sys.exit(1)
- 
+
     # 获取accessToken
     accessToken = get_access_token()
     # 接收的用户
     users = config["user"]
     # 传入地区获取天气信息
     region = config["region"]
-    weather, temp, wind_dir = get_weather(region)
+    weather, temp, wind_dir, future_temp = get_weather(region)
     note_ch = config["note_ch"]
     note_en = config["note_en"]
+    test_day = config['test_day']
+    year = localtime().tm_year
+    month = localtime().tm_mon
+    day = localtime().tm_mday
+    today = datetime.date(datetime(year=year, month=month, day=day))
+    test_day_str = get_birthday(test_day, year, today)
     if note_ch == "" and note_en == "":
         # 获取词霸每日金句
         note_ch, note_en = get_ciba()
     # 公众号推送消息
     for user in users:
-        send_message(user, accessToken, region, weather, temp, wind_dir, note_ch, note_en)
+        send_message(user, accessToken, region, weather, temp, wind_dir, note_ch, note_en, future_temp, test_day_str)
     os.system("pause")
